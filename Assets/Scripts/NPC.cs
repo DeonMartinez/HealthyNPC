fdb950a558ea032c044bd956cd0097d4c1f47fa6
DeonMartinez <49213704+DeonMartinez@users.noreply.github.com>
2020-11-06 20:26:03 -0700

    Add files via upload

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float turnSpeed = 90f;
+    [SerializeField] private int startingHp = 100;
+    [SerializeField] private UnityEngine.UI.Slider hpBarSlider = null;
+    [SerializeField] private ParticleSystem deathParticlePrefab = null;
+    [SerializeField] private int currentHp;
+
+    internal void TakeDamage(int amount)
+    {
+        GetComponent<IHealth>().TakeDamage(amount);
+    }
+
+    private void Update()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        transform.Rotate(0f, turnSpeed * Time.deltaTime, 0f);
+        hpBarSlider.transform.LookAt(Camera.main.transform);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(startingHp / 10);
+        }
+    }
+
+}
